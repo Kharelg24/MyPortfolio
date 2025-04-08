39e4307f39bb7289c19515c6d0103b3aeb1257f6
Kharelg24 <Kharelg24@vt.edu>
2025-04-08 16:41:01 -0400

    Added about me description

--- src/components/Personal.jsx
+++ src/components/Personal.jsx
@@ -18,6 +18,9 @@ function Personal(){
 
             <section className="aboutMe">
                 <h2>About Me</h2>
+                <p>
+                I’m someone who always strives for the best. I’m motivated, ambitious, and I like to think I’m hard-working. Staying fit and eating clean is a big part of my life—I love working out and keeping my nutrition on point. I’m always looking for ways to improve myself and learn something new. I enjoy being around people, trying new food, and exploring different places. I’m really into experiencing new cultures, and one of my goals is to visit all 7 wonders of the world. So far, I’ve checked off two, but there will be more to come! 
+                </p>
                 
             </section>
             </div>
