d09601f4e994cb2487ee263f2de9e5be506e8009
Kharelg24 <Kharelg24@vt.edu>
2025-04-09 16:19:48 -0400

    Intigrated gita API

--- src/backend/aboutMe.js
+++ src/backend/aboutMe.js
@@ -1,14 +1,19 @@
 import express from "express";
 import data from "../database/workHistory.json" with {type: "json"};
 import cors from "cors";
+import axios from "axios";
 import projectData from "../database/technicalProjects.json" with {type: "json"};
 import certificationData from "../database/certifications.json" with {type: "json"};
+
+import {bhagavadGitaAPIOptions} from './apiOptions';
 
 const app = express();
 const PORT = 4000;
 
 app.use(express.json());
 app.use(cors());
+
+let gitaCachedData = null;
 
 // this is to read the work history database
 app.get('/data', (req, res) => {
@@ -25,6 +30,28 @@ app.get('/projectData', (req, res) => {
 app.get('/certificationData', (req, res) => {
     res.json(certificationData);
 });
+
+// this is to fetch the gita api
+app.get('/chapters', async (req, res) => {
+    try{
+
+        if (cachedData){
+            console.log('Returning cached data');
+            return res.json(cachedData);
+        }
+
+        const response = await axios.request(bhagavadGitaAPIOptions);
+
+        cachedData = response.data;
+
+        res.json(response.data);
+
+    }catch(error){
+        console.error('Error fetching Bhagvad Gita Chapters:', error);
+        res.status(500).json( {error: 'Failed to fetch data from the API' });
+    }
+});
+
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
